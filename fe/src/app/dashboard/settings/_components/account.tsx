dccb365cd9016f0c1c35667fcf2d2cbb7216cf66
dev-mahfuj80 <mahfuj@nexstack.sg>
2025-05-27 20:35:40 +0600

    feat: implement account settings tab with user and organization details display

--- fe/src/app/dashboard/settings/_components/account.tsx
+++ fe/src/app/dashboard/settings/_components/account.tsx
@@ -37,23 +37,81 @@ export default function AccountTab() {
   // }
   // Put all those value in ui
   return (
+    // need to seperate 2 section user and organaizatin information
+
     <Card>
-      <div className="p-6 space-y-6">
-        <h2 className="text-lg font-medium">Account</h2>
-        <div className="flex items-center gap-2">
-          <p>Username :</p>
-          <p className="text-muted-foreground italic">{user?.name}</p>
+      <div className="p-6 space-y-6 flex flex-col gap-6">
+        <div>
+          <h2 className="text-xl font-medium">Account</h2>
+          <div className="flex items-center gap-2">
+            <p>Username :</p>
+            <p className="text-muted-foreground italic">{user?.name}</p>
+          </div>
+          <div className="flex items-center gap-2">
+            <p>Email :</p>
+            <p className="text-muted-foreground italic">
+              {user?.email}
+              <span className="ml-2 text-xs italic">Can&apos;t be changed</span>
+            </p>
+          </div>
+          <div className="flex items-center gap-2">
+            <p>Role :</p>
+            <p className="text-muted-foreground italic">{user?.role}</p>
+          </div>
+          <div className="flex items-center gap-2">
+            <p>Organizations :</p>
+            <p className="text-muted-foreground italic">
+              {user?.organizations?.length}
+            </p>
+          </div>
+          <div className="flex items-center gap-2">
+            <p>Created At :</p>
+            <p className="text-muted-foreground italic">{user?.createdAt}</p>
+          </div>
         </div>
-        <div className="flex items-center gap-2">
-          <p>Email :</p>
-          <p className="text-muted-foreground italic">
-            {user?.email}
-            <span className="ml-2 text-xs italic">Can&apos;t be changed</span>
-          </p>
-        </div>
-        <div className="flex items-center gap-2">
-          <p>Role :</p>
-          <p className="text-muted-foreground italic">{user?.role}</p>
+        <div>
+          <h2 className="text-xl font-medium mb-4 grid gap-4 md:grid-cols-4">
+            Organizations
+          </h2>
+          {user?.organizations?.map((organization) => (
+            <div key={organization.id}>
+              <p>{organization.name}</p>
+              <p>{organization.website}</p>
+              <p>{organization.category}</p>
+              <p>{organization.location}</p>
+              <p>{organization.description}</p>
+              <p>{organization.established}</p>
+              <p>{organization.size}</p>
+              <p>{organization.employees}</p>
+              <p>{organization.turnover}</p>
+              <p>{organization.revenue}</p>
+              <p>{organization.profit}</p>
+              <p>{organization.marketArea}</p>
+            </div>
+          ))}
+
+          <div className="flex items-center gap-2">
+            <p>Updated At :</p>
+            <p className="text-muted-foreground italic">{user?.updatedAt}</p>
+          </div>
+          <div className="flex items-center gap-2">
+            <p>Facebook Token :</p>
+            <p className="text-muted-foreground italic">
+              {user?.facebookToken}
+            </p>
+          </div>
+          <div className="flex items-center gap-2">
+            <p>Linkedin Token :</p>
+            <p className="text-muted-foreground italic">
+              {user?.linkedInAccessToken}
+            </p>
+          </div>
+          <div className="flex items-center gap-2">
+            <p>Email Verified :</p>
+            <p className="text-muted-foreground italic">
+              {user?.emailVerified ? "Yes" : "No"}
+            </p>
+          </div>
         </div>
       </div>
     </Card>
